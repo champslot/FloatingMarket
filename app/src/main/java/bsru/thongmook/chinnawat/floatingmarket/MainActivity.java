fc7c8f6ad948a46ba14268f45def96bdd4af9daa
champslot <champchinz@gmail.com>
2017-02-23 18:49:12 +0700

    Initial commit

---
+++ app/src/main/java/bsru/thongmook/chinnawat/floatingmarket/MainActivity.java
@@ -0,0 +1,13 @@
+package bsru.thongmook.chinnawat.floatingmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

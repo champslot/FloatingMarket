5e2fe5f51ac92469b4fa2cd84502a3500399e5ff
champslot <champchinz@gmail.com>
2017-02-23 18:59:05 +0700

    create sign up page

--- app/src/main/java/bsru/thongmook/chinnawat/floatingmarket/SignUpActivity.java
+++ app/src/main/java/bsru/thongmook/chinnawat/floatingmarket/SignUpActivity.java
@@ -3,13 +3,15 @@ package bsru.thongmook.chinnawat.floatingmarket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
-    } // main method
 
-}   //main class
+    }   // main method
+
+
+}   // main class

28dbcd4189aac688457b51bc1972e8bd55352945
champslot <champchinz@gmail.com>
2017-04-25 00:14:48 +0700

    checkuserpass  doing v2

--- app/src/main/java/bsru/thongmook/chinnawat/floatingmarket/MainActivity.java
+++ app/src/main/java/bsru/thongmook/chinnawat/floatingmarket/MainActivity.java
@@ -3,9 +3,14 @@ package bsru.thongmook.chinnawat.floatingmarket;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private Button signInButton, signUpButton, skipButton;
     private EditText userEditText, passEditText;
     private String  userString, passString;
-    private static final String urlPHP = "http://swiftcodingthai.com/bsru/get_user_champ.php";
-
+    private String[] loginStrings = new String[6]; // ข้อมูลใน db
+    private static final String urlPHP = "http://swiftcodingthai.com/bsru/get_user.php";
+    private boolean aBoolean = true;
 
 
     @Override
@@ -70,13 +76,54 @@ public class MainActivity extends AppCompatActivity {
 
             GetUser getUser = new GetUser(MainActivity.this);
             getUser.execute(urlPHP);
-            String strJSON =
+            String strJSON = getUser.get();
+            Log.d("2404v1", "strJSON==>" + strJSON);
 
+            JSONArray jsonArray = new JSONArray(strJSON);//ตัดคำ
+            for (int i = 0; i < jsonArray.length(); i += 1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (userString.equals(jsonObject.getString("user_name"))) {
+                    loginStrings[0] = jsonObject.getString("id");
+                    loginStrings[1] = jsonObject.getString("user_name");
+                    loginStrings[2] = jsonObject.getString("user_pass");
+                    loginStrings[3] = jsonObject.getString("name");
+                    loginStrings[4] = jsonObject.getString("email");
+                    loginStrings[5] = jsonObject.getString("Lat");
+                    loginStrings[6] = jsonObject.getString("Lng");
+
+                    aBoolean = false;
+
+                } // if
+
+            } // for loop
+
+            if (aBoolean) {
+                //user false
+                MyAlert myAlert = new MyAlert(MainActivity.this);
+                myAlert.myDialog("ไม่พบ User", "ไม่มี " + userString + " ในระบบ ");
+
+            } else if (!passString.equals(loginStrings[2])) {
+                // password false
+                MyAlert myAlert = new MyAlert(MainActivity.this);
+                myAlert.myDialog("Wrong Password", "Please Try Agian Wrong Password");
+            } else {
+                //  password pass
+                Toast.makeText(MainActivity.this, " Welcome " + loginStrings[3],
+                        Toast.LENGTH_SHORT).show();
+
+                //  intent to menu
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                intent.putExtra("Login", loginStrings);
+                startActivity(intent);
+                finish();
+            }
+
+        } catch (Exception e) {
+            Log.d("2404V1", "e checkUserPass ==> " + e.toString());
         }
 
-
-
-    }
+    }   //check user pass
 
 
 
